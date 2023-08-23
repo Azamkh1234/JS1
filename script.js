e702d3168019c8a4ee726379d20bdaee809f1dc2
azam <azamhonkhodjaev@gmail.com>
2023-08-23 13:24:07 +0500

    first

---
+++ script.js
@@ -0,0 +1,14 @@
+var str = prompt('Введите свое имя')
+console.log(str);
+var year = prompt('Ваш возвраст')
+console.log(year);
+var plus = prompt('6+2')
+console.log( 'Пример 1: ' + ('6 + 2 = ' + 8 + ' Ваш ответ ' + plus));
+var minus = prompt('')
+console.log( 'Пример 2: ' + ('10 - 5 = ' + 5 + ' Ваш ответ ' + minus));
+var delenie = prompt('')
+console.log( 'Пример 3: ' + ('24 / 4 = ' + 6 + ' Ваш ответ ' + delenie));
+var umnozenie = prompt('')
+console.log( 'Пример 4: ' + ('2 * 5 = ' + 10 + ' Ваш ответ ' + umnozenie));
+var octatok = prompt('')
+console.log( 'Пример 5: ' + ('15 % 4 = ' + 3 + ' Ваш ответ ' + octatok));
